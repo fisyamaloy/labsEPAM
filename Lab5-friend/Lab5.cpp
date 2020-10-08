543483701b341d41704673daf6e7b23a00de2f23
fisyamaloy <fisyamaloy@users.noreply.github.com>
2020-10-09 01:43:19 +0300

    changed labs names and added lab on friend-functions

---
+++ Lab5-friend/Lab5.cpp
@@ -0,0 +1,87 @@
+﻿#include <iostream>
+#include <iostream>
+#include <string>
+#include <assert.h>
+#include <ctime>
+
+using namespace std;
+
+class Array {
+public:
+	Array(const int rowsAmount, const int columnsAmount) {
+		assert(rowsAmount > 0 && columnsAmount > 0);
+		this->rowsAmount = rowsAmount;
+		this->columnsAmount = columnsAmount;
+		fillArray();
+		showArray();
+	}
+
+	~Array() {
+		for (int i = 0; i < rowsAmount; i++) {
+			delete[] A[i];
+		}
+
+		delete[] A;
+	}
+
+	void showArray() {
+		for (int i = 0; i < rowsAmount; i++) {
+			for (int j = 0; j < columnsAmount; j++) {
+				cout << A[i][j] << '\t';
+			}
+			cout << endl;
+		}
+	}
+	
+	friend int getNumberOfFirstNegativeRow(Array& arr);
+
+private:
+	int** A;
+	unsigned int rowsAmount;
+	unsigned int columnsAmount;
+
+	void fillArray() {
+		srand(time(0));
+		A = new int*[rowsAmount];
+		for (int i = 0; i < rowsAmount; i++) {
+			A[i] = new int[columnsAmount];
+			for (int j = 0; j < columnsAmount; j++) {
+				A[i][j] = rand() % 30 - 20;
+			}
+		}
+	}
+};
+
+
+/*
+* Функция возвращает индекс первой строки двумерного массива, которая не имеет положительных элементов.
+* Функция возвращает -1, если в каждой строке массива существует положительный элемент.
+*/
+int getNumberOfFirstNegativeRow(Array& arr) {
+	for (int i = 0; i < arr.rowsAmount; i++) {
+		bool rowHasPositiveElem = false;
+		for (int j = 0; j < arr.columnsAmount; j++) {
+			if (arr.A[i][j] > 0) {
+				rowHasPositiveElem = true;
+				break;
+			}
+		}
+
+		if (!rowHasPositiveElem) {
+			return i;
+		}
+	}
+
+	return -1;
+}
+
+int main() {
+	Array array(3, 4);
+	
+	int index = getNumberOfFirstNegativeRow(array);
+	if (index != -1) {
+		cout << "index of first row, in which all numbers are negative is " << index << endl;
+	} else {
+		cout << "All rows of arrays contain positive number" << endl;
+	}
+}
---
+++ Lab6-generics/ConsoleApplication2.cpp
@@ -0,0 +1,84 @@
+﻿#include <iostream>
+#include <ctime>
+#include <assert.h>
+#include <vector>
+#include <string>
+#define epsilon 0.00001
+
+using namespace std;
+
+template<class T>
+void fillMassive(T*& arr, int& length) {
+    srand(time(0));
+    length = rand() % 10 + 2;
+    arr = new int[length];
+    for (int i = 0; i < length; i++) {
+        arr[i] = 1.0 * (rand() % 100 - 50);
+    }
+}
+
+template<class T>
+constexpr void showMassive(T*& arr, const unsigned int length) {
+    for (int i = 0; i < length; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+template<class T>
+constexpr T getMax(T firstValue, T secondValue) {
+    return firstValue - secondValue > epsilon ? firstValue : secondValue;
+}
+
+template<class T>
+constexpr T getMax(T*& A, int length) {
+    assert(length > 0);
+    int max = A[0];
+    for (int i = 1; i < length; i++) {
+        if (max - A[i] < epsilon)
+            max = A[i];
+    }
+
+    return max;
+}
+
+
+string getMax(const vector<string> rows, const int length) {
+    assert(length > 0);
+    int maxElemIndex = 0;
+    for (int i = 1; i < length; i++) {
+        if (rows[maxElemIndex] < rows[i])
+            maxElemIndex = i;
+    }
+
+    return rows[maxElemIndex];
+}
+
+
+int main() {
+    setlocale(LC_ALL, "ru");
+
+    double a;
+    double b;
+    cin >> a;
+    cin >> b;
+
+    cout << "max number is " << getMax(a, b) << endl;
+
+    int length;
+    int* arr;
+    fillMassive(arr, length);
+    showMassive(arr, length);
+    cout << "max number is " << getMax(arr, length) << endl;
+
+    const int rowsAmount = 5;
+    vector<string> rows;
+    for (int i = 0; i < rowsAmount; i++) {
+        string row;
+        getline(cin,row);
+        rows.push_back(row);
+    }
+
+    cout << "Строка максимальной длины: " << endl;
+    cout << getMax(rows, rowsAmount) << endl;
+}
